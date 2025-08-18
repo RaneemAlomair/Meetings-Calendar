2cba6b607293c045609306f705e5d96a49dd3f1d
RaneemAlomair <raneemalomair11@gmail.com>
2025-08-18 13:39:07 +0300

    Initial Commit

---
+++ Meetings Calendar/Meetings_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meetings_CalendarApp.swift
+//  Meetings Calendar
+//
+//  Created by Raneem Alomair on 24/02/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Meetings_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meetings CalendarTests/Meetings_CalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meetings_CalendarTests.swift
+//  Meetings CalendarTests
+//
+//  Created by Raneem Alomair on 24/02/1447 AH.
+//
+
+import Testing
+@testable import Meetings_Calendar
+
+struct Meetings_CalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
